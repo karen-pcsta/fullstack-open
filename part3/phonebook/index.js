59544eb07aeb9577147c7e065ceee0c828abbb40
Karen Costa <karen.pcosta@gmail.com>
2023-04-09 02:49:54 -0300

    Initial commit

---
+++ part3/phonebook/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const date = new Date();
+
+app.use(express.json());
+
+let contacts = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(contacts);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`Phonebook has info for ${contacts.length} people.
+    
+   ${date}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = contacts.find((person) => person.id === id);
+
+  if (!person) {
+    res.status(404).send;
+  }
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  contacts = contacts.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
