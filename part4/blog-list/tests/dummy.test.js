559024b9c4c339b9f6a40c37b6ea0768b2ba9dab
Karen Costa <karen.pcosta@gmail.com>
2023-06-02 02:11:13 -0300

    Add solution for exercise 4.4

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -1,7 +1,42 @@
-const listHelper = require("../utils/list_helper")
+const { dummy, totalLikes } = require("../utils/list_helper")
 
 test("dummy returns one", () => {
   const blogs = []
-  const result = listHelper.dummy(blogs)
+  const result = dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ]
+
+  test("of empty list is zero", () => {
+    const result = totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test("of a bigger list is calculated right", () => {
+    const result = totalLikes([
+      { likes: 1 },
+      { likes: 2 },
+      { likes: 3 },
+      { likes: 4 },
+      { likes: 5 },
+      { likes: 6 },
+    ])
+    expect(result).toBe(21)
+  })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    const sumLikes = blogs.reduce((acc, current) => acc + current.likes, 0)
+    return sumLikes
+  }
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
