8350f93f1c93649d9036b050c8dad5a5acf285b2
Karen Costa <karen.pcosta@gmail.com>
2023-08-20 01:16:11 -0300

    Add solution for exercise 4.19

--- part4/blog-list/controllers/blogLists.js
+++ part4/blog-list/controllers/blogLists.js
@@ -12,6 +12,14 @@ let exportedList = []
 //   exportedList = res.json(blogList)
 // })
 
+const getTokenFrom = (req) => {
+  const authorization = req.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "")
+  }
+  return null
+}
+
 blogListsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   res.json(blogs)
@@ -19,16 +27,18 @@ blogListsRouter.get("/", async (req, res) => {
 
 blogListsRouter.post("/", async (req, res) => {
   const { body } = req
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: "token invalid" })
+  }
 
-  const getUsers = await User.find({})
-
-  const user = getUsers[0]
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({ ...body, user: user.id })
 
   const savedBlog = await blog.save()
 
-  user.blogs = user.blogs.concat(savedBlog._id)
+  user.blogs = user.blogs.concat(savedBlog.id)
 
   await user.save()
 
